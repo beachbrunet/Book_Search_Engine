920e9e4ec960111dc09cf49bc8dfef913d6a56e1
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-07 19:32:06 -0600

    added user and login mutation

--- server/config/schema/resolvers.js
+++ server/config/schema/resolvers.js
@@ -5,26 +5,54 @@ const { signToken } = require("../utils/auth");
 
 // example 23 outline
 const resolver = {
+    // me query
   Query: {
-    me:async (parent , args, contect) => {
+    me: async (parent , args, contect) => {
         if (context.user) {
-            const userData = await User.findOne({ _id:: context.user_id}).select('-_v -password');
+            const userData = await User.findOne({ _id: context.user_id}).select('-_v -password');
             return userData;
         }
         throw new AuthenticationError ('User not logged in');
     },
   },
 }
+// Mutations
+// adding a user
+// Login
+// save book
+
+Mutation: {
+// adding a user
+addUSer: async (parent, args)
+ => {
+     const user = await User.create(args);
+     const token = something(user);
+
+     return { token, user};
+ }
+ login: async (parent, { email, password }) => {
+    const user = await User.findOne({ email });
+    
+    if(!user) {
+        throw new AuthenticationError ('Incorrect credentials');
+    }
+    const correctPw = await user.isCorrectPassword(password);
+
+    if (!correctPw) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+
+    const token = signToken(user);
+
+    return { token, user };
+  }
+};
 
 
 
 
+// example code
 
 
-
-
-// mutation add user/login/savebook
-
-// remove book
 
 module.exports = resolvers;
