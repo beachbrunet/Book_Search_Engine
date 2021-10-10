15d7604d835d74344d68ce08fc0e4b32be8a8f88
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-10 12:59:07 -0600

    worked with project group on login

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
-import { createUser } from "../utils/API";
+// import { createUser } from "../utils/API";
 import Auth from "../utils/auth";
 
 // add mutation here
 import { ADD_USER } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
 
 const SignupForm = () => {
   // set initial form state
@@ -23,6 +24,7 @@ const SignupForm = () => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -32,21 +34,6 @@ const SignupForm = () => {
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
-    }
-
-    try {
-      const response = await createUser(...userFormData);
-
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      // const { token, user } = await response.json();
-      // console.log(user);
-      // Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
     }
 
     setUserFormData({
